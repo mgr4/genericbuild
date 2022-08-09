bee53a2172c06e07a7c3b4e771bace15771c45b1
suvsap <suv@EPINBANW01B8.localdomain>
2022-08-09 18:30:42 +0530

    added genericbuild script

---
+++ vars/genericbuild.groovy
@@ -0,0 +1,28 @@
+def call(Map config=[:]{
+node {
+    stage('SCM') {
+	echo 'Gathering code from version control'
+        git branch: '${branch}', url: 'https://github.com/suvsap/JenkinsGroovy.git'
+    }
+    stage('Build') {
+       try{
+        echo 'Building....'
+        echo 'Building New Feature'
+        releasenotes()
+    }catch(ex){
+        echo 'Something went wrong...'
+        echo ex.toString()
+        currentBuild.result = 'Failure'
+    }
+     finally{
+        //cleanup
+     }
+   }
+    stage('Test') {
+        echo 'Testing....'
+    }
+    stage('Deploy') {
+        echo 'Deploying....'
+    }
+ }
+}
